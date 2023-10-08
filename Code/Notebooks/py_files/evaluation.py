5ad3c67a4b714e98f71b63f129eb2681a3143ea1
Davide Rigamonti <80167579+daviderigamonti@users.noreply.github.com>
2023-10-08 18:27:03 +0200

    Added evaluation and visualization functions

---
+++ Code/Notebooks/py_files/evaluation.py
@@ -0,0 +1,224 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+
+from tqdm import tqdm
+
+# Generate the embeddings and the corresponding dataset reference for an image dataset
+def generate_image_embeddings(
+    image_encoder,                 # Image encoder of clip model
+    dataset_eval,                  # Dataset to generate embeddings (WARNING: the dataset must not be shuffling or have a shuffle buffer size of 1)
+    dataset_pred_map=lambda *x: x, # Lambda mapping function for prediction
+    dataset_ref_map=lambda *x: x,  # Lambda mapping function for reference
+):
+    print("Generating image embeddings")
+    # Generate image embedding
+    image_embeddings = image_encoder.predict(
+        dataset_eval.map(dataset_pred_map),
+        verbose=1,
+    )
+    # Construct reference dataset for retrieving side data of elements
+    dataset_reference = [e for e in dataset_eval.map(dataset_ref_map).unbatch()]
+    return dataset_reference, image_embeddings
+
+# Generate the embeddings and the corresponding dataset reference for a text dataset
+def generate_text_embeddings(
+    text_encoder,                  # Image encoder of clip model
+    dataset_eval,                  # Dataset to generate embeddings (WARNING: the dataset must not be shuffling or have a shuffle buffer size of 1)
+    dataset_pred_map=lambda *x: x, # Lambda mapping function for prediction
+    dataset_ref_map=lambda *x: x,  # Lambda mapping function for reference
+):
+    print("Generating image embeddings")
+    # Generate text embedding
+    text_embeddings = text_encoder.predict(
+        dataset_eval.map(dataset_pred_map),
+        verbose=1,
+    )
+    # Construct reference dataset for retrieving side data of elements
+    dataset_reference = [e for e in dataset_eval.map(dataset_ref_map).unbatch()]
+    return dataset_reference, text_embeddings
+
+# Return the results in the form of reference dataset indexes of a text to image retrieval for a series of queries
+def find_t2i_matches(
+    queries,                # Queries to search
+    text_encoder,           # Text encoder of clip model
+    image_embeddings,       # Generated image embeddings
+    k=10,                   # Number of elements for top-k
+    normalize=True,         # Embedding normalization
+):
+    print("Computing Text-to-Image matches")
+    query_embedding = text_encoder.predict(queries)
+    # Normalize the query and the image embeddings
+    if normalize:
+        image_embeddings = tf.math.l2_normalize(image_embeddings, axis=1)
+        query_embedding = tf.math.l2_normalize(query_embedding, axis=1)
+    # Compute the dot product between the query and the image embeddings
+    dot_similarity = tf.matmul(query_embedding, image_embeddings, transpose_b=True)
+    # Retrieve top k indices
+    results = tf.math.top_k(dot_similarity, k).indices.numpy()
+    return results
+
+# Return the results in the form of reference dataset indexes of a image to text retrieval for a series of queries
+def find_i2t_matches(
+    queries,                # Queries to search
+    image_encoder,          # Text encoder of clip model
+    text_embeddings,        # Generated image embeddings
+    k=10,                   # Number of elements for top-k
+    normalize=True,         # Embedding normalization
+):
+    print("Computing Image-to-Text matches")
+    query_embedding = image_encoder.predict(queries)
+    # Normalize the query and the text embeddings
+    if normalize:
+        text_embeddings = tf.math.l2_normalize(text_embeddings, axis=1)
+        query_embedding = tf.math.l2_normalize(query_embedding, axis=1)
+    # Compute the dot product between the query and the text embeddings
+    dot_similarity = tf.matmul(query_embedding, text_embeddings, transpose_b=True)
+    # Retrieve top k indices
+    results = tf.math.top_k(dot_similarity, k).indices.numpy()
+    return results
+
+# Extract the reference dataset objects given a list of indexes
+def index_to_reference(results, dataset_reference):
+    return [[dataset_reference[match] | {"index": match} for match in result] for result in results]
+
+# Transform a one-hot encoded list of boolean concepts to the respective list of raw concepts labels
+# If the flag string_form is set to true, the returned list will contain strings of concatenated concept text
+def decode_concepts(concepts, encoder, concept_list, string_form=True):
+    c = np.array(concepts)
+    c = encoder.inverse_transform(c)
+    if string_form:
+        c = [" ".join([concept_list[concept] for concept in e]) for e in c]
+    return c
+
+
+# Retrieve relevant items given a list of queries (DO NOT RUN THIS ON A COMPLETE DATASET!!!)
+def retrieve_relevant(queries, reference_preprocess=lambda x: x, relevance=lambda m, o: m == o):
+    return [
+        [element for element in map(reference_preprocess, dataset_reference) if relevance(query, element)]
+        for query in queries
+    ]
+
+# Compute the number of relevant items in the first k matches in a list of results
+# If queries is None, it is assumed that the queries ran to obtain the list of results are parallel to the elements in dataset_reference
+def compute_relevant_at_k(results, dataset_reference, queries=None, k=None, reference_preprocess=lambda x: x, relevance=lambda m, o: m == o):
+    if not k:
+        k = len(results[0])
+    if queries:
+        relevant_reference = retrieve_relevant(queries, reference_preprocess=reference_preprocess, relevance=relevance)
+    else:
+        relevant_reference = map(reference_preprocess, dataset_reference)
+    return [ 
+        np.count_nonzero([relevance(match, reference) for match in list(map(reference_preprocess, matches))[0:k]])
+        for matches, reference in zip(results, relevant_reference) 
+    ]
+
+# Computes the total number of relevant elements for a dataset or queries
+# It is assumed that the element returned by reference_preprocess is hashable and can be used as a dictionary key
+# If queries is None, it is assumed that the queries ran to obtain the list of results are parallel to the elements in dataset_reference
+def compute_total_relevance(
+    dataset_reference, queries=None,                                   # Dataset reference or queries to compute total relevance for
+    reference_preprocess=lambda x: x, relevance=lambda m, o: m == o,   # Preprocessing function and relevance function
+    load_from_file=True, save_to_file=True,                            # Load/Save flags
+    fileinfo={}                                                        # Info for loading/saving data from/to a file in the form of a dictionary with the following keys:
+                                                                       # path, filename, test_split, val_split, split, metric, other
+                                                                       # if a filename is specified, only the base path is needed
+):
+    tot_relevant = True
+    # Check if queries are passed, if so run general function without loading/saving to file
+    if queries:
+        relevant_reference = retrieve_relevant(queries, reference_preprocess=reference_preprocess, relevance=relevance)
+        return [len(e) for e in relevant_reference]
+    # Check for existing file and load relevance data
+    if load_from_file:
+        tot_relevant = load_relevance_from_csv(fileinfo)
+        if not tot_relevant:
+            print("Proceeding with total relevance calculation...")
+    if not tot_relevant:
+        # Build preprocessed dataset
+        relevant_reference = list(map(reference_preprocess, dataset_reference))
+        total_n = {}
+        # Iterate through dataset and count equal items
+        for element in relevant_reference:
+            if element in total_n:
+                total_n[element] += 1
+            else:
+                total_n[element] = 1
+        # Check bytecode of relevance function to determine if the relevance function is equality,
+        # if so, return counts, otherwise apply relevance to the whole dataset
+        if not relevance.__code__.co_code == (lambda m, o: m == o).__code__.co_code:
+            total_n = {element: sum([total_n[x] for x in total_n if relevance(x, element) and element != x]) + 1 for element in tqdm(total_n)} 
+        tot_relevant = [total_n[element] for element in relevant_reference]
+    # Check for existing file and save relevance data
+    if save_to_file:
+        save_relevance_to_csv(tot_relevant, fileinfo)
+    return tot_relevant
+
+# Build the filename for a relevance file given some dataset and preprocessing attributes
+def build_relevance_filename(
+    path,                              # Base path for the file
+    filename=None,                     # Name of the csv file to load, if None it will be inferred from dataset attributes
+    test_split=0.2,                    # Test split percentage
+    val_split=0,                       # Validation split percentage
+    split="train",                     # Either "train", "test" or "val"
+    metric="",                         # Metric used to compute relevance
+    other=[],                          # Other attributes as an ordered list of (name, value) tuples
+):
+    if not filename:
+        filename = "TotRelevant_" + str(test_split) + "_" + str(val_split) + "_" + split + "_" + metric
+        for attr in other:
+            filename += "_" + attr[0] + "-" + str(attr[1])
+        filename += ".csv"
+    filename = path + filename
+    return filename
+
+# Load a csv relevance file given a filename or some dataset and preprocessing attributes
+def load_relevance_from_csv(fileinfo={}):
+    filename = build_relevance_filename(**fileinfo)
+    if not os.path.exists(filename):
+        print(f"The relevance file \"{filename}\" does not exist!")
+        return False
+    else:
+        try:
+            return np.squeeze(pd.read_csv(filename, header=None).values.tolist())
+        except OSError as error:
+            print(f"Couldn't load file \"{filename}\": {error}")
+    return False
+
+# Save total relevant data to a csv relevance file given a filename or some dataset and preprocessing attributes
+def save_relevance_to_csv(tot_relevant, fileinfo={}):
+    filename = build_relevance_filename(**fileinfo)
+    if os.path.exists(filename):
+        print(f"Overwriting \"{filename}\" relevance file!")
+    df = pd.DataFrame(tot_relevant)
+    try:
+        df.to_csv(filename, index=False, header=False)
+        return True
+    except OSError as error:
+            print(f"Couldn't save file \"{filename}\": {error}")
+    return False
+
+
+def compute_top_k_accuracy(results, dataset_reference, relevant_at_k):
+    hits = np.count_nonzero(relevant_at_k)
+    return hits / len(dataset_reference)
+
+def compute_map_k(results, dataset_reference, relevant_at_k, k=None):
+    if not k:
+        k = len(results[0])
+    precision_at_k = [r/k for r in relevant_at_k]
+    return np.sum(precision_at_k) / len(dataset_reference)
+
+def compute_mar_k(results, dataset_reference, relevant_at_k, total_relevant):
+    recall_at_k = [rk/tr for rk, tr in zip(relevant_at_k, total_relevant)]
+    return np.sum(recall_at_k) / len(dataset_reference)
+
+def compute_F1_k(precision=0, recall=0):
+    if precision + recall == 0:
+        f1_score = 0
+    else:
+        f1_score = 2 * (precision * recall) / (precision + recall)
+        return f1_score
---
+++ Code/Notebooks/py_files/visualization.py
@@ -0,0 +1,189 @@
+import random
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import plotly.graph_objects as go
+
+# Visualize results for text to image queries
+def visualize_t2i_results(query, matches):
+    print("Top matches for query: \"" + query + "\"")
+    if "image path" in matches[0]:
+        plt.figure(figsize=(18, 18))
+    for i in range(len(matches)):
+        if "image path" in matches[i]:
+            path = matches[i]["image path"].numpy().decode('UTF-8')
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(mpimg.imread(path))
+            plt.axis("off")
+        if "caption" in matches[i]:
+            caption = matches[i]["caption"].numpy().decode('UTF-8')
+            print(f"{i}) {caption}")
+        
+# Standard visualization for a multi-purpose plotly graph
+def visualize_multigraph(functions, titlexyf=(None, None, None), legend=True):
+    fig = go.Figure()
+    for function in functions:
+        x = function['x']
+        y = function['y']
+        label = function['label'] if 'label' in function else ""
+        color = function['color'] if 'color' in function else None
+        linestyle = function['style'] if 'style' in function else "solid"
+        marker = go.scatter.Marker(symbol=function['marker'], size=10) if 'marker' in function else None
+        opacity = function['opacity'] if 'opacity' in function else 1
+        k = len(x)
+        fig.add_trace(go.Scatter(
+            x=x, y=y,
+            line=go.scatter.Line(color=color, dash=linestyle),
+            opacity=opacity,
+            marker=marker,
+            mode="lines+markers+text" if marker else "lines+text",
+            name=label,
+        ))
+    fig.update_xaxes(
+        title=titlexyf[0],
+        ticks="outside", ticklen=8, minor=dict(dtick=0.5, ticklen=6, tickcolor="black", showgrid=True), ticklabelstep=1, dtick=1, 
+        range=(1,k), 
+    )
+    fig.update_yaxes(
+        title=titlexyf[1],
+        ticks="outside", ticklen=8, minor=dict(dtick=0.01, ticklen=6, tickcolor="black", showgrid=True), ticklabelstep=1, dtick=0.1,
+    )
+    fig.update_layout(
+        title=titlexyf[2],
+        width=900, height=600,
+        margin=dict(l=50, r=50, b=20, t=40, pad=4),
+        paper_bgcolor="LightSteelBlue",
+    )
+    fig.show()
+
+
+# Compute baselines for retrieval
+# Assumption of sampling with repetitions, results get more inaccurate as k/l -> inf
+def retrieval_baselines(dataset_reference, total_relevant, k, metrics=[]):
+    l = len(dataset_reference)
+    metrics_out = {}
+    for metric in metrics:
+        if metric["id"] == METRIC_ACCURACY:
+            metrics_out[METRIC_ACCURACY] = sum([ 1 - pow((l - n_el) / l, k) for n_el in total_relevant]) / l
+        elif metric["id"] == METRIC_MAP:
+            metrics_out[METRIC_MAP] = sum([ n_el / l for n_el in total_relevant]) / l
+        elif metric["id"] == METRIC_MAR:
+            metrics_out[METRIC_MAR] = sum([ k / l for n_el in total_relevant]) / l
+        elif metric["id"] == METRIC_F1:
+            metrics_out[METRIC_F1] = compute_F1_k(metrics_out[METRIC_MAP], metrics_out[METRIC_MAR])
+    return metrics_out
+    
+# Computation of a retrieval report containing metrics
+def retrieval_report(
+    results, reference, relevant,   # Task results, dataset reference and relevant hits at k for task
+    tot_relevant=None,              # Rotal number of relevant elements for each dataset element
+    k=None,                         # k for metrics computation (should be less or equal than k of retrieval)
+    baselines=True,                 # Calculate baselines alongside metrics
+    metrics=[],                     # Metrics to take into consideration
+    output=True,                    # Print outputs to stdout
+    title="Retrieval Report",       # Title of the report
+    decimal_precision=4,            # Decimal precision of values
+):
+    if not k:
+        k = len(results[0])
+    metrics_out = {}
+    
+    for metric in metrics:
+        if metric["id"] == METRIC_ACCURACY:
+            metrics_out[METRIC_ACCURACY] = compute_top_k_accuracy(results, reference, relevant)
+        elif metric["id"] == METRIC_MAP:
+            metrics_out[METRIC_MAP] = compute_map_k(results, reference, relevant, k=k)
+        elif metric["id"] == METRIC_MAR:
+            metrics_out[METRIC_MAR] = compute_mar_k(results, reference, relevant, tot_relevant)
+        elif metric["id"] == METRIC_F1:
+            metrics_out[METRIC_F1] = compute_F1_k(metrics_out[METRIC_MAP], metrics_out[METRIC_MAR])
+            
+    if baselines:
+            baselines = retrieval_baselines(reference, tot_relevant, k, metrics=metrics)
+            
+    if output:
+        print(f"\n ### {title} ###")
+        for metric in metrics:
+            string = f"{metric['name']:<30}: {round(metrics_out[metric['id']] * 100, decimal_precision):10}%"
+            if baselines:
+                string += f"{'   Baseline':<8}: {round(baselines[metric['id']] * 100, decimal_precision):10}%"
+            print(string)
+    
+    if baselines:
+        return metrics_out, baselines
+    return metrics_out
+        
+# Computation of a retrieval report in graph form containing metrics 
+def retrieval_graph_report(
+    results, reference,                 # Task results and dataset reference
+    tot_relevant=None,                  # Total number of relevant elements for each dataset element
+    k_range=(1, 10),                    # k range for metrics computation (maximum value shoul not be greater than k of retrieval)
+    baselines=True,                     # Calculate baselines alongside metrics
+    metrics=[],                         # Metrics to take into consideration
+    titlexyf=(None, None, None),        # Tuple containing: (title of x axis, title of y axis, figure title)
+    reference_preprocess=lambda x: x,   # Function to preprocess data contained in the reference dataset
+    relevance=lambda m, o: m == o,      # Function to compare elements
+    functions=None,                     # Plot pre-existing function data
+):
+    if not functions:
+        functions = {metric["id"]: {"x": [], "y": [], "label": metric["id"], "color": metric["color"], "marker": "0", "opacity": 0.8} for metric in metrics}
+        if baselines:
+            functions |= {metric["id"] + "_base": {"x": [], "y": [], "label": metric["id"] + " Baseline", "color": metric["color"], "style": "dash", "opacity": 0.5} for metric in metrics}
+        for k in range(k_range[0], k_range[1] + 1):
+            relevant = compute_relevant_at_k(results, reference, k=k, reference_preprocess=reference_preprocess, relevance=relevance)
+            report = retrieval_report(results, reference, relevant, tot_relevant, k=k, baselines=baselines, metrics=metrics, output=False)
+            metrics_out = report[0] if baselines else report
+            baselines = report[1] if baselines else None
+            for metric in metrics_out:
+                functions[metric]["x"].append(k)
+                functions[metric]["y"].append(metrics_out[metric])
+                if baselines:
+                    functions[metric + "_base"]["x"].append(k)
+                    functions[metric + "_base"]["y"].append(baselines[metric])
+    visualize_multigraph(functions.values(), titlexyf)
+    return functions
+
+# Computation of a retrieval report in graph form containing metrics, comparing multiple models on the same dataset
+def retrieval_graph_compare(
+    multi_results, reference,           # Per-model task results and dataset reference
+    model_ids,                          # List of ordered model ids and labels in the form {"id": id, "label": label} 
+    tot_relevant=None,                  # Total number of relevant elements for each dataset element
+    k_range=(1, 10),                    # k range for metrics computation (maximum value shoul not be greater than k of retrieval)
+    metrics=[],                         # Metrics to take into consideration
+    titlexyf=(None, None, None),        # Tuple containing: (title of x axis, title of y axis, figure title)
+    reference_preprocess=lambda x: x,   # Function to preprocess data contained in the reference dataset
+    relevance=lambda m, o: m == o,      # Function to compare elements
+    functions=None,                     # Plot pre-existing function data
+):
+    if not functions:
+        # Add random markers
+        markers = ["0", "1", "2", "3", "17", "26"]
+        if len(markers) < len(model_ids):
+            print("Too many models!")
+            return None
+        markers = random.sample(markers, len(model_ids))
+        model_ids = [model | {"marker": marker} for model, marker in zip(model_ids, markers)]
+        # Generate function models
+        functions = {
+            metric["id"] + model["id"]: 
+            {"x": [], "y": [], "label": model["label"] + " " + metric["id"], "color": metric["color"], "marker": model["marker"], "opacity": 0.8} 
+            for model in model_ids for metric in metrics
+        }
+        # Fill functions
+        for model, results in zip(model_ids, multi_results):
+            for k in range(k_range[0], k_range[1] + 1):
+                relevant = compute_relevant_at_k(results, reference, k=k, reference_preprocess=reference_preprocess, relevance=relevance)
+                metrics_out = retrieval_report(results, reference, relevant, tot_relevant, k=k, baselines=False, metrics=metrics, output=False)
+                for metric in metrics_out:
+                    functions[metric + model["id"]]["x"].append(k)
+                    functions[metric + model["id"]]["y"].append(metrics_out[metric])
+    visualize_multigraph(functions.values(), titlexyf)
+    return functions
+    
+# Manually compute some text to image queries
+def manual_t2i_queries(queries, text_encoder, image_embeddings, dataset_reference, k=10, normalize=True):
+    results = find_t2i_matches(queries, clip_text_encoder, test_image_embeddings, k=k, normalize=normalize)
+    results = index_to_reference(results, test_dataset_reference)
+    for query, matches in zip(queries, results):
+        visualize_t2i_results(query, matches)
