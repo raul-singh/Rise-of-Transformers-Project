cec0be4b6cf4422431f4a27343552302019e52c0
davide-rigamonti-polimi <davide2.rigamonti@mail.polimi.it>
2023-11-26 11:11:45 +0100

    Removed leftovers of old TTA

--- Code/Notebooks/py_files/clip.py
+++ Code/Notebooks/py_files/clip.py
@@ -5,15 +5,24 @@ import yaml
 import tensorflow_hub as hub
 import tensorflow_text as text
 
-from py_files.daug import RandomSwapWords, RandomConceptWords
-
 tfk = tf.keras
 tfkl = tf.keras.layers
 kb = tf.keras.backend
 
 
+class EncoderTTA(tfk.Model):
+    def __init__(self, encoder, tta_pipeline, tta_n=10, **kwargs):
+        super().__init__(**kwargs)
+        self.encoder = encoder
+        self.tta_pipeline = tta_pipeline
+        self.tta_n = tta_n
+        
+    def call(self, features, training=False):
+        return tf.math.reduce_mean(tf.stack([self.encoder(self.tta_pipeline(features)) for _ in range(self.tta_n)], axis=-1), axis=-1)
+
+
 class CLIP(tfk.Model):
-    def __init__(self, image_encoder, text_encoder, image_tta=None, text_tta=None, tta_n=10, **kwargs):
+    def __init__(self, image_encoder, text_encoder, **kwargs):
         super().__init__(**kwargs)
         self.image_encoder = image_encoder
         self.text_encoder = text_encoder
@@ -25,10 +34,6 @@ class CLIP(tfk.Model):
             trainable=True
         )
 
-        self.image_tta = image_tta
-        self.text_tta = text_tta
-        self.tta_n = tta_n
-
         # Necessary for initialization
         self.initialize_model()
 
@@ -38,6 +43,11 @@ class CLIP(tfk.Model):
         return [self.loss_tracker]
 
     def call(self, features, training=False):
+
+        image_emb = self.image_encoder(features["image"], training=training)
+        text_emb = self.text_encoder(features["caption"], training=training)
+
+        return image_emb, text_emb
         
         if self.image_tta and not training:
             image_emb = tf.math.reduce_mean(tf.stack([self.image_encoder(self.image_tta(features["image"])) for _ in range(self.tta_n)]))
@@ -48,7 +58,7 @@ class CLIP(tfk.Model):
         else:
             text_emb = self.text_encoder(features["caption"], training=training)
         
-        return image_emb, text_emb
+        
 
     def CLIP_loss(self, image_emb, text_emb):
         norm_image_emb = tf.math.l2_normalize(image_emb, axis=1)
@@ -176,23 +186,12 @@ def build_clip(settings_path, weights_path=None, load_weights=True):
     model_settings = model['model']
     image_shape = tuple(model_settings['img_input_shape'])
 
-    image_tta = tfk.Sequential([eval(tta_setting) for tta_setting in model_settings['image_tta']]) if 'image_tta' in model_settings else None
-    text_tta = tfk.Sequential([eval(tta_setting) for tta_setting in model_settings['text_tta']]) if 'text_tta' in model_settings else None
-    tta_n = model_settings['tta_n'] if 'tta_n' in model_settings else None
-
     print('Building clip...')
 
     clip_text_encoder = text_encoder(model_settings['embed_dim'], text_preprocess, text_transformer)
     clip_image_encoder = image_encoder(image_shape, model_settings['embed_dim'], img_supernet, img_preprocess)
 
-
-    clip = CLIP(
-        clip_image_encoder, 
-        clip_text_encoder, 
-        image_tta,
-        text_tta,
-        tta_n
-        )
+    clip = CLIP(clip_image_encoder, clip_text_encoder)
     
     clip.compile(
         optimizer = tf.optimizers.AdamW(
